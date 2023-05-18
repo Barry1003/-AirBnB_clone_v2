b65449ee9e9fa49ff824e0a633496d9b823d886c
Barry1003 <111708006+Barry1003@users.noreply.github.com>
2023-05-18 21:06:57 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,14 +5,11 @@ Your web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
-
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
